5d352df921ed66dc45890f900f0f96ed753864e7
matias <matias.quintana.r@gmail.com>
2018-05-27 16:59:16 -0400

    rearranged file structure

---
+++ lib/PS2KybdTX/PS2KybdTX.ino
@@ -0,0 +1,93 @@
+
+
+/*  PS2Keyboard library example
+  
+  PS2Keyboard now requries both pins specified for begin()
+
+  keyboard.begin(data_pin, irq_pin);
+  
+  Valid irq pins:
+     Arduino:      2, 3
+     Arduino Mega: 2, 3, 18, 19, 20, 21
+     Teensy 1.0:   0, 1, 2, 3, 4, 6, 7, 16
+     Teensy 2.0:   5, 6, 7, 8
+     Teensy++ 1.0: 0, 1, 2, 3, 18, 19, 36, 37
+     Teensy++ 2.0: 0, 1, 2, 3, 18, 19, 36, 37
+     Sanguino:     2, 10, 11
+  
+  for more information you can read the original wiki in arduino.cc
+  at http://www.arduino.cc/playground/Main/PS2Keyboard
+  or http://www.pjrc.com/teensy/td_libs_PS2Keyboard.html
+  
+  Like the Original library and example this is under LGPL license.
+  
+  Modified by Cuninganreset@gmail.com on 2010-03-22
+  Modified by Paul Stoffregen <paul@pjrc.com> June 2010
+  Modified by Steve Spence <sspence@green-trust.org> September 20013
+*/
+   
+#include <PS2Keyboard.h>
+#include <SoftwareSerial.h>
+SoftwareSerial mySerial(11, 10); // RX, TX
+
+const int DataPin = 4;
+const int IRQpin =  3;
+
+PS2Keyboard keyboard;
+
+void setup() {
+  delay(1000);
+  keyboard.begin(DataPin, IRQpin);
+  Serial.begin(9600);
+  mySerial.begin(9600);
+  Serial.println("Keyboard Test:");
+}
+
+void loop() {
+  
+  // mySerial.println("T");
+  
+  if (keyboard.available()) {
+    
+    // read the next key
+    char c = keyboard.read();
+    
+    // check for some of the special keys
+    if (c == PS2_ENTER) {
+      mySerial.println();
+      Serial.println();
+    } else if (c == PS2_TAB) {
+      mySerial.print("[Tab]");
+      Serial.print("[Tab]");
+    } else if (c == PS2_ESC) {
+      mySerial.print("[ESC]");
+      Serial.print("[ESC]");
+    } else if (c == PS2_PAGEDOWN) {
+      mySerial.print("[PgDn]");
+      Serial.print("[PgDn]");
+    } else if (c == PS2_PAGEUP) {
+      mySerial.print("[PgUp]");
+      Serial.print("[PgUp]");
+    } else if (c == PS2_LEFTARROW) {
+      mySerial.print("[Left]");
+      Serial.print("[Left]");
+    } else if (c == PS2_RIGHTARROW) {
+      mySerial.print("[Right]");
+      Serial.print("[Right]");
+    } else if (c == PS2_UPARROW) {
+      mySerial.print("[Up]");
+      Serial.print("[Up]");
+    } else if (c == PS2_DOWNARROW) {
+      mySerial.print("[Down]");
+      Serial.print("[Down]");
+    } else if (c == PS2_DELETE) {
+      mySerial.print("[Del]");
+      Serial.print("[Del]");
+    } else {
+      
+      // otherwise, just print all normal characters
+      mySerial.print(c);
+      Serial.print(c);
+    }
+  }
+}
---
+++ src/PS2keyboard/PS2keyboard.ino
@@ -0,0 +1,100 @@
+/** 
+Título: Programa para imprimir con impresora térmica lo introducido
+       con un teclado PS/2 y una pantalla LCD como visualizador
+Autor: Ing. Matias Quintana
+Fecha: 04/07/2015
+**/
+
+#include <SoftwareSerial.h>
+#include <PS2Keyboard.h>
+#include <LiquidCrystal.h>
+
+const int DataPin = 3;
+const int IRQpin =  2;
+
+PS2Keyboard keyboard;
+SoftwareSerial Thermal(4, 5);
+LiquidCrystal lcd(12, 11, 10, 9, 8, 7);
+
+int zero=0;
+int heatTime = 80;
+int heatInterval = 255;
+char printDensity = 15; 
+char printBreakTime = 15;
+int i=0; //Ubicación de cursor
+char oracion[30];
+char impresion[30];
+
+void setup() {
+  delay(1000);
+  keyboard.begin(DataPin, IRQpin);
+  Thermal.begin(19200); //To write to our new printer
+  initPrinter();
+  //Set up the LCD's number of columns and rows:
+  lcd.begin(16, 2);
+  //Print a header to the LCD.
+  lcd.print("    NoteTaker");  
+}
+
+void initPrinter() {
+ //Modify the print speed and heat
+ Thermal.write(27);
+ Thermal.write(55);
+ Thermal.write(7); //Default 64 dots = 8*('7'+1) 
+ Thermal.write(heatTime); //Default 80 or 800us
+ Thermal.write(heatInterval); //Default 2 or 20us 
+ //Modify the print density and timeout
+ Thermal.write(18);
+ Thermal.write(35); 
+ int printSetting = (printDensity<<4) | printBreakTime;
+ Thermal.write(printSetting); //Combination of printDensity and printBreakTime
+}
+
+void loop() {
+  //Set the cursor to column 0, line 1
+  //(Note: line 1 is the second row, since counting begins with 0)
+  lcd.setCursor(i, 1);
+  //Turn on the blinking cursor
+  lcd.blink();
+  
+  if (keyboard.available()) {
+    //Read the next key
+    char c = keyboard.read();
+    if (c!= NULL) {
+      if(i >= 15){
+        lcd.scrollDisplayLeft();
+        }
+        
+      if (c == PS2_DELETE){
+        if(oracion[16] != NULL ){lcd.scrollDisplayRight();}
+        oracion[i-1]=32;
+        lcd.print(oracion[i-1]);
+        i--;
+      }
+      
+      else{
+      oracion[i]=c;
+      lcd.print(oracion[i]);
+      i++;
+      }
+    }
+    //Check for some of the special keys
+    if (c == PS2_ENTER) {
+      lcd.clear();
+      lcd.print("    NoteTaker");
+      // height/width enlarge
+      Thermal.write(29); 
+      Thermal.write(33);
+      Thermal.write(255);  
+      Thermal.print(oracion);
+      Thermal.write(10);
+      
+      for(int x=0;x<=i;x++){
+        oracion[x]=NULL;
+      }
+      
+      delay(3000);
+      i=0;
+    } 
+  }
+}
